d35a4d59db2efe604ce9a92fc3ec6047c1082f2f
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-03-08 13:16:35 -0300

    App pronto

--- app/src/main/java/com/example/limonada/MainActivity.kt
+++ app/src/main/java/com/example/limonada/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -25,12 +24,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.limonada.ui.theme.LimonadaTheme
@@ -49,6 +46,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun LimonadaApp() {
     var atual by remember { mutableIntStateOf(1) }
+    var contagem by remember { mutableIntStateOf(1) }
     Box(
         modifier = Modifier
             .fillMaxSize(),
@@ -64,35 +62,59 @@ fun LimonadaApp() {
             fontSize = 18.sp,
             text = stringResource(R.string.limonada)
         )
-        Column(
-        ) {
+        Column{
             when (atual) {
                 1 -> {
                     LimonadaComBotaoImagemTexto(
                         textId = R.string.step1,
                         imageId = R.drawable.lemon_tree,
-                        contentDescriptionId = R.string.tree_description
+                        contentDescriptionId = R.string.tree_description,
+                        onImageClick =
+                        {
+                            atual = 2
+                        }
                     )
                 }
 
                 2 -> {
-
+                    LimonadaComBotaoImagemTexto(
+                        textId = R.string.step2,
+                        imageId = R.drawable.lemon_squeeze,
+                        contentDescriptionId = R.string.lemon_description,
+                        onImageClick =
+                        {
+                            atual = 2
+                            contagem = (1..4).random()
+                            if (contagem==4)
+                            {
+                                atual = 3
+                            }
+                        }
+                    )
                 }
 
                 3 -> {
-
+                    LimonadaComBotaoImagemTexto(
+                        textId = R.string.step3,
+                        imageId = R.drawable.lemon_drink,
+                        contentDescriptionId = R.string.drink_description,
+                        onImageClick =
+                        {
+                            atual = 4
+                        }
+                    )
                 }
 
                 4 -> {
-
-                }
-
-                5 -> {
-
-                }
-
-                6 -> {
-
+                    LimonadaComBotaoImagemTexto(
+                        textId = R.string.step4,
+                        imageId = R.drawable.lemon_restart,
+                        contentDescriptionId = R.string.tree_description,
+                        onImageClick =
+                        {
+                            atual = 1
+                        }
+                    )
                 }
             }
         }
@@ -104,6 +126,7 @@ fun LimonadaComBotaoImagemTexto(
     textId: Int,
     imageId: Int,
     contentDescriptionId: Int,
+    onImageClick: () -> Unit,
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -111,7 +134,7 @@ fun LimonadaComBotaoImagemTexto(
         modifier = Modifier.fillMaxSize()
     )
     {
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = onImageClick) {
             Image(
                 painter = painterResource(id = imageId),
                 contentDescription = stringResource(id = contentDescriptionId)
